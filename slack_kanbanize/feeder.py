9bbe036a8087ab8fd506e823108fb77734867428
mportela <marcel.portela@gmail.com>
2014-10-01 00:26:31 -0300

    + implemented tdd of _get_kambanize_board_activities + fix some docstrings

--- slack_kanbanize/feeder.py
+++ slack_kanbanize/feeder.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+from python_kanbanize.wrapper import Kanbanize
+from pyslack import SlackClient
+
 
 class Feeder(object):
 
@@ -8,7 +11,7 @@ class Feeder(object):
                  kambanize_timedelta_collect_minutes=2,
                  local_timediff=-3):
         """
-            parameters:
+            Arguments:
             @kambanize_api_key - kambanize appi key to be used
             @kambanize_board_id - kambanize board_id to be monitored
             @kambanize_timedelta_collect_minutes - minutes to collect data from
@@ -16,9 +19,10 @@ class Feeder(object):
             @slack_token - slack token autorization to be used
             @slack_channel - slack channel to be used
             @slack_user - slack user to be used
-            @local_timediff - diference in local time hours from utc
+            @local_timediff - diference in local time hours from utc to present
+                              datetime data returned
 
-            * - in slack free version there is a limit of "Limit per hour (per
+            obs: in slack free version there is a limit of "Limit per hour (per
                 API KEY)" of 30 calls
                 see https://kanbanize.com/ctrl_integration/ for details
         """
@@ -33,3 +37,13 @@ class Feeder(object):
             'channel': slack_channel,
             'user': slack_user
         }
+
+    def _get_kambanize_board_activities(self, from_date, to_date):
+        """
+            Used to get python-kambanize.get_board_activities
+            Arguments:
+            @from_date - datetime object to be used in get_board_activities
+            @to_date - datetime object to be used in get_board_activities
+            Return a list with board activities
+        """
+        pass
--- slack_kanbanize/tests.py
+++ slack_kanbanize/tests.py
@@ -1,15 +1,23 @@
 # coding=utf-8
 
 import unittest
+import mock
+import datetime
 
+from dateutil import tz
 import feeder
+from python_kanbanize.wrapper import Kanbanize
 
 
-class TestSequenceFunctions(unittest.TestCase):
+class TestFeederClass(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = feeder.Feeder("foo_kambanize_api_key", 3, "foo_slack_token",
+                                 "foo_slack_channel")
+        self.utc_zone = tz.tzutc()
+        self.local_zone = tz.tzlocal()
 
     def test_basic_instance(self):
-        obj = feeder.Feeder("foo_kambanize_api_key", 3, "foo_slack_token",
-                            "foo_slack_channel")
         exp_kambanize_opts = {
             'api_key': "foo_kambanize_api_key",
             'board_id': 3,
@@ -20,9 +28,31 @@ class TestSequenceFunctions(unittest.TestCase):
             'channel': "foo_slack_channel",
             'user': "slackbot"
         }
-        self.assertEqual(exp_kambanize_opts, obj.kambanize_opts)
-        self.assertEqual(exp_slack_opts, obj.slack_opts)
-        self.assertEqual(-3, obj.local_timediff)
+        self.assertEqual(exp_kambanize_opts, self.obj.kambanize_opts)
+        self.assertEqual(exp_slack_opts, self.obj.slack_opts)
+        self.assertEqual(-3, self.obj.local_timediff)
+
+    @mock.patch.object(Kanbanize, 'get_board_activities')
+    def test_get_kambanize_board_activities(self, mk_get_activities):
+        to_date = datetime.datetime.now()
+        from_date = datetime.datetime.now() - datetime.timedelta(hours=1)
+
+        mk_ret = [{'type': 'blah'}, {'type': 'blah'}, {'type': 'blah2'}]
+
+        mk_get_activities.return_value = mk_ret
+        ret = self.obj._get_kambanize_board_activities(from_date, to_date)
+
+        exp_from_date = from_date.replace(tzinfo=self.local_zone)
+        exp_from_date = exp_from_date.astimezone(self.utc_zone).strftime(
+                                                        "%Y-%m-%d %H:%M:%S")
+        exp_to_date = to_date.replace(tzinfo=self.local_zone)
+        exp_to_date = exp_to_date.astimezone(self.utc_zone).strftime(
+                                                        "%Y-%m-%d %H:%M:%S")
+        mk_get_activities.assert_called_once_with(
+                                        self.obj.kambanize_opts['board_id'],
+                                        exp_from_date,
+                                        exp_to_date)
+        self.assertEqual(mk_ret, ret)
 
 if __name__ == '__main__':
     unittest.main()
