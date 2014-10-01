cfcc754ff00b5c09cd813dadc540f991e7fb9d8e
mportela <marcel.portela@gmail.com>
2014-10-01 11:16:56 -0300

    * fix typo kanbanize word..

--- slack_kanbanize/feeder.py
+++ slack_kanbanize/feeder.py
@@ -12,15 +12,15 @@ class Feeder(object):
     UTC_ZONE = tz.tzutc()
     LOCAL_ZONE = tz.tzlocal()
 
-    def __init__(self, kambanize_api_key, kambanize_board_id, slack_token,
+    def __init__(self, kanbanize_api_key, kanbanize_board_id, slack_token,
                  slack_channel, slack_user='slackbot',
-                 kambanize_timedelta_collect_minutes=2,
+                 kanbanize_timedelta_collect_minutes=2,
                  local_timediff=-3):
         """
             Arguments:
-            @kambanize_api_key - kambanize appi key to be used
-            @kambanize_board_id - kambanize board_id to be monitored
-            @kambanize_timedelta_collect_minutes - minutes to collect data from
+            @kanbanize_api_key - kanbanize appi key to be used
+            @kanbanize_board_id - kanbanize board_id to be monitored
+            @kanbanize_timedelta_collect_minutes - minutes to collect data from
                                                   N minutes before now
             @slack_token - slack token autorization to be used
             @slack_channel - slack channel to be used
@@ -33,10 +33,10 @@ class Feeder(object):
                 see https://kanbanize.com/ctrl_integration/ for details
         """
         self.local_timediff = local_timediff
-        self.kambanize_opts = {
-            'api_key': kambanize_api_key,
-            'board_id': kambanize_board_id,
-            'collect_minutes_timedelta': kambanize_timedelta_collect_minutes
+        self.kanbanize_opts = {
+            'api_key': kanbanize_api_key,
+            'board_id': kanbanize_board_id,
+            'collect_minutes_timedelta': kanbanize_timedelta_collect_minutes
         }
         self.slack_opts = {
             'token': slack_token,
@@ -44,15 +44,15 @@ class Feeder(object):
             'user': slack_user
         }
 
-    def _get_kambanize_board_activities(self, from_date, to_date):
+    def _get_kanbanize_board_activities(self, from_date, to_date):
         """
-            Used to get python-kambanize.get_board_activities
+            Used to get python-kanbanize.get_board_activities
             Arguments:
             @from_date - datetime object to be used in get_board_activities
             @to_date - datetime object to be used in get_board_activities
             Return a list with board activities
         """
-        kambanize = Kanbanize(self.kambanize_opts['api_key'])
+        kanbanize = Kanbanize(self.kanbanize_opts['api_key'])
 
         from_date_aware = from_date.replace(tzinfo=Feeder.LOCAL_ZONE)
         from_dt_utc_string = from_date_aware.astimezone(
@@ -63,6 +63,6 @@ class Feeder(object):
                                                     Feeder.UTC_ZONE).strftime(
                                                            "%Y-%m-%d %H:%M:%S")
 
-        return kambanize.get_board_activities(self.kambanize_opts['board_id'],
+        return kanbanize.get_board_activities(self.kanbanize_opts['board_id'],
                                               from_dt_utc_string,
                                               to_dt_utc_string)
--- slack_kanbanize/tests.py
+++ slack_kanbanize/tests.py
@@ -12,14 +12,14 @@ from python_kanbanize.wrapper import Kanbanize
 class TestFeederClass(unittest.TestCase):
 
     def setUp(self):
-        self.obj = feeder.Feeder("foo_kambanize_api_key", 3, "foo_slack_token",
+        self.obj = feeder.Feeder("foo_kanbanize_api_key", 3, "foo_slack_token",
                                  "foo_slack_channel")
         self.utc_zone = tz.tzutc()
         self.local_zone = tz.tzlocal()
 
     def test_basic_instance(self):
-        exp_kambanize_opts = {
-            'api_key': "foo_kambanize_api_key",
+        exp_kanbanize_opts = {
+            'api_key': "foo_kanbanize_api_key",
             'board_id': 3,
             'collect_minutes_timedelta': 2
         }
@@ -28,19 +28,19 @@ class TestFeederClass(unittest.TestCase):
             'channel': "foo_slack_channel",
             'user': "slackbot"
         }
-        self.assertEqual(exp_kambanize_opts, self.obj.kambanize_opts)
+        self.assertEqual(exp_kanbanize_opts, self.obj.kanbanize_opts)
         self.assertEqual(exp_slack_opts, self.obj.slack_opts)
         self.assertEqual(-3, self.obj.local_timediff)
 
     @mock.patch.object(Kanbanize, 'get_board_activities')
-    def test_get_kambanize_board_activities(self, mk_get_activities):
+    def test_get_kanbanize_board_activities(self, mk_get_activities):
         to_date = datetime.datetime.now()
         from_date = datetime.datetime.now() - datetime.timedelta(hours=1)
 
         mk_ret = [{'type': 'blah'}, {'type': 'blah'}, {'type': 'blah2'}]
 
         mk_get_activities.return_value = mk_ret
-        ret = self.obj._get_kambanize_board_activities(from_date, to_date)
+        ret = self.obj._get_kanbanize_board_activities(from_date, to_date)
 
         exp_from_date = from_date.replace(tzinfo=self.local_zone)
         exp_from_date = exp_from_date.astimezone(self.utc_zone).strftime(
@@ -49,7 +49,7 @@ class TestFeederClass(unittest.TestCase):
         exp_to_date = exp_to_date.astimezone(self.utc_zone).strftime(
                                                         "%Y-%m-%d %H:%M:%S")
         mk_get_activities.assert_called_once_with(
-                                        self.obj.kambanize_opts['board_id'],
+                                        self.obj.kanbanize_opts['board_id'],
                                         exp_from_date,
                                         exp_to_date)
         self.assertEqual(mk_ret, ret)
